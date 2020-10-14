cc1115d872163b3b83a10323b4d7bdee817067f5
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2020-10-14 10:39:19 +0530

    jack coffee
    best website for coffee cafe.

---
+++ customjs.js
@@ -0,0 +1,11 @@
+console.log("we are connected");
+
+
+
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.carousel').carousel();
+
+});
